885fa1910bb6041be1ff512e55607a63c9c86dca
Wang Siye <siye.wang@suse.com>
2026-03-19 14:48:23 +0800

    PANDARIA: go generate

--- pkg/apis/ack.pandaria.io/v1/types.go
+++ pkg/apis/ack.pandaria.io/v1/types.go
@@ -76,6 +76,8 @@ type ACKClusterConfigSpec struct {
 	ClusterIsUpgrading  bool    `json:"clusterIsUpgrading"`
 	TaskId              string  `json:"taskId"`
 	Addons              []Addon `json:"addons,omitempty" norman:"noupdate"`
+	// 创建新的 vpc vswitch
+	ZoneIDs []string `json:"zoneIds,omitempty" norman:"noupdate"`
 }
 
 type ACKClusterConfigStatus struct {
@@ -94,9 +96,17 @@ type NodePoolInfo struct {
 	/* node_pool_info */
 	NodepoolId string `json:"nodepool_id,omitempty"`
 	Name       string `json:"name,omitempty"`
+
 	/* auto_scaling */
-	InstancesNum          int64  `json:"instances_num,omitempty"`
-	ScalingType           string `json:"scaling_type,omitempty"`
+	InstancesNum int64  `json:"instances_num,omitempty"`
+	ScalingType  string `json:"scaling_type,omitempty"`
+
+	// 开启/关闭自动扩容（nil = 未设置，保持旧逻辑 Enable=false），因为想要更清晰的区分设置了还是为设置所以用了 *
+	AutoScalingEnabled *bool `json:"auto_scaling_enabled,omitempty"`
+	// 覆盖 min/max（nil = 未设置，用 InstancesNum）
+	MinInstances *int64 `json:"min_instances,omitempty"`
+	MaxInstances *int64 `json:"max_instances,omitempty"`
+
 	IsBondEip             bool   `json:"is_bond_eip,omitempty" norman:"noupdate"`
 	EipInternetChargeType string `json:"eip_internet_charge_type,omitempty" norman:"noupdate"`
 	EipBandwidth          int64  `json:"eip_bandwidth,omitempty" norman:"noupdate"`
